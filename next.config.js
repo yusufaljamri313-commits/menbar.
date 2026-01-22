04cdee694654740c2ab8ce703c8f6b3e3ef5b530
banijamraclub <yusufaljamri313@gmail.com>
2026-01-22 15:56:32 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ]
+  },
+};
+
+module.exports = nextConfig;
